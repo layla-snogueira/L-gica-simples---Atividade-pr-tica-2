b2503b5452ad8021d275d6532038487de7570693
Layla <laylas.snogueira@gmail.com>
2023-01-22 17:46:50 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// Questão 1 //
+const idade= 24;
+
+document.write (`<strong>Questão 1</strong>
+<p>Minha idade é de ${idade}</p>
+<br>`);
+
+
+//Questão 2// 
+const num1 = 2;
+const num2 = 4;
+const soma = num1+num2;
+
+document.write (`<strong>Questão 2</strong>
+<p>O valor da soma entre o número 1 e o número 2 é de: ${soma}</p>
+<br>`);
+
+//Questão 3 //
+
+const valorcompra= 142.50;
+const numeroparcelas= 3;
+const valorparcela= valorcompra/numeroparcelas;
+
+document.write (`<strong> Questão 3 </strong>
+<p>O valor total da compra foi de:$${valorcompra}</p>
+<p>Forma de pagamento:${numeroparcelas}x de $${valorparcela}</p>
+<br>`);
+
+//Questão 4//
+const minutos = 120
+const segundos = minutos*60
+
+document.write (`<strong> Questão 4 </strong>
+<p>${minutos} minutos equivalem à ${segundos} segundos!</p>
+<br>`);
+
+//Questão 5//
+const nomealuno = 'Pedro';
+const nota1 = 8.5;
+const nota2 = 7.9;
+const nota3 = 6.7;
+const media = (nota1+nota2+nota3)/3;
+
+document.write (`<strong> Questão 5 </strong>
+<p>O aluno${nomealuno} ficou com média ${media}.</p>
+<br>`);
